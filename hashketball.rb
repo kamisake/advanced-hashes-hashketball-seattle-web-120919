1f054ac9a4842576ee6e12b2a54620e34aa4a974
Charles Camisa <charles@kalekoko.com>
2019-12-05 19:31:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,8 +45,8 @@ def game_hash
 
       { player_name: "Jason Terry",
         number: 31,
-        shoe: 19,
-        points: 15,
+        shoe: 15,
+        points: 19,
         rebounds: 2,
         assists: 2,
         steals: 4,
