794345f1037051c38471c2e6bd98a5758c1c08aa
Charles Camisa <charles@kalekoko.com>
2019-12-05 18:57:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   {
-    home: {team_name: "Brooklyn Nets"
-    colors: ["Black", "White"]
+    home: {team_name: "Brooklyn Nets",
+    colors: ["Black", "White"],
     players: [
       { player_name: "Alan Anderson",
         number: 0,
